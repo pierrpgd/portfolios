45765024ca77055c4636b297cec929af0719dd9b
Pierrick <pierrick.pagaud@gmail.com>
2025-05-14 15:26:23 +0200

    Dev - The name on the homepage is a variable

--- pierrpgd/views.py
+++ pierrpgd/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def home(request, name="Pierrick Pagaud"):
+    context={'name':name}
+    return render(request, 'home.html', context)
