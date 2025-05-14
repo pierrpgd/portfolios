1c7527fc430db6a41525dbc2c338bd93095ed71a
Pierrick <pierrick.pagaud@gmail.com>
2025-05-14 13:17:55 +0200

    Test - Le titre est 'Portfolio de Pierrick Pagaud'

--- pierrpgd/tests.py
+++ pierrpgd/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from selenium import webdriver
+import unittest
 
-# Create your tests here.
+class HomePageTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_project_is_installed_and_working(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn("Portfolio de Pierrick Pagaud", self.browser.title)
