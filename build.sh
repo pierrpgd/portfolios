06d9eaabb0eb9f7167be6af78bf1f88482776a5b
Pierrick <pierrick.pagaud@gmail.com>
2025-05-26 15:05:33 +0200

    Correction - Migrations are made before the creation of superuser

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Apply any outstanding database migrations
+python manage.py makemigrations
+python manage.py migrate
+
 # Create superuser if CREATE_SUPERUSER is set
 if [[ $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input
@@ -12,7 +16,3 @@ fi
 
 # Convert static asset files
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-python manage.py makemigrations
-python manage.py migrate
