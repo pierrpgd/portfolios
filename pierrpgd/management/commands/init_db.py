10575b1afb4fdebb0bdc1d2b3ead0407910abf8c
Pierrick <pierrick.pagaud@gmail.com>
2025-06-04 22:30:56 +0200

    Creation of Catalina portfolio

--- pierrpgd/management/commands/init_db.py
+++ pierrpgd/management/commands/init_db.py
@@ -28,86 +28,88 @@ class Command(BaseCommand):
             with open(os.path.join(os.path.dirname(__file__), 'init_db.json'), 'r', encoding='utf-8') as f:
                 data = json.load(f)
 
-            # Vérifier si le profil existe déjà
-            if Profile.objects.filter(identifiant=data["profile"]["identifiant"]).exists():
-                self.stdout.write("Profile already exists")
-                return
+            for person in data:
 
-            # Création du profil
-            self.stdout.write("Création du profil...")
-            profile = Profile.objects.create(
-                name=data["profile"]["name"],
-                identifiant=data["profile"]["identifiant"],
-                title=data["profile"]["title"]
-            )
-            self.stdout.write(f"Profil créé avec l'ID: {profile.id}")
+                # Vérifier si le profil existe déjà
+                if Profile.objects.filter(identifiant=person["profile"]["identifiant"]).exists():
+                    self.stdout.write("Profile already exists")
+                    return
 
-            # Création des compétences
-            self.stdout.write("Création des compétences...")
-            for skill_data in data["skills"]:
-                if not Skill.objects.filter(name=skill_data["name"], category=skill_data["category"]).exists():
-                    Skill.objects.create(category=skill_data["category"], name=skill_data["name"])
-                skill = Skill.objects.get(name=skill_data["name"], category=skill_data["category"])
-                ProfileSkill.objects.create(profile=profile, skill=skill, level=skill_data["level"])
-                self.stdout.write(f"Compétence créée: {skill.name} (ID: {skill.id})")
-
-            # Création des sections A propos
-            self.stdout.write("Création des sections À propos...")
-            for content in data["about"]:
-                about = About.objects.create(profile=profile, content=content)
-                self.stdout.write(f"Section À propos créée (ID: {about.id})")
-
-            # Création des expériences
-            self.stdout.write("Création des expériences...")
-            for exp_data in data["experiences"]:
-                exp = Experience.objects.create(
-                    profile=profile,
-                    dates=exp_data["dates"],
-                    position=exp_data["position"],
-                    company=exp_data["company"],
-                    location=exp_data["location"],
-                    description=exp_data["description"],
-                    details=exp_data["details"],
-                    url=exp_data["url"]
+                # Création du profil
+                self.stdout.write("Création du profil...")
+                profile = Profile.objects.create(
+                    name=person["profile"]["name"],
+                    identifiant=person["profile"]["identifiant"],
+                    title=person["profile"]["title"]
                 )
-                skill_ids = [Skill.objects.get(name=skill_name).id for skill_name in exp_data["skills"]]
-                exp.skills.add(*skill_ids)
-                self.stdout.write(f"Expérience créée (ID: {exp.id})")
+                self.stdout.write(f"Profil créé avec l'ID: {profile.id}")
 
-            # Création des éducations
-            self.stdout.write("Création des éducations...")
-            for edu_data in data["educations"]:
-                edu = Education.objects.create(
-                    profile=profile,
-                    dates=edu_data["dates"],
-                    title=edu_data["title"],
-                    institution=edu_data["institution"],
-                    field=edu_data["field"],
-                    location=edu_data["location"],
-                    description=edu_data["description"],
-                    details=edu_data["details"],
-                    url=edu_data["url"]
-                )
-                skill_ids = [Skill.objects.get(name=skill_name).id for skill_name in edu_data["skills"]]
-                edu.skills.add(*skill_ids)
-                self.stdout.write(f"Éducation créée (ID: {edu.id})")
+                # Création des compétences
+                self.stdout.write("Création des compétences...")
+                for skill_data in person["skills"]:
+                    if not Skill.objects.filter(name=skill_data["name"], category=skill_data["category"]).exists():
+                        Skill.objects.create(category=skill_data["category"], name=skill_data["name"])
+                    skill = Skill.objects.get(name=skill_data["name"], category=skill_data["category"])
+                    ProfileSkill.objects.create(profile=profile, skill=skill, level=skill_data["level"])
+                    self.stdout.write(f"Compétence créée: {skill.name} (ID: {skill.id})")
 
-            # Création des projets
-            self.stdout.write("Création des projets...")
-            for project_data in data["projects"]:
-                project = Project.objects.create(
-                    profile=profile,
-                    title=project_data["title"],
-                    description=project_data["description"],
-                    details=project_data["details"],
-                    image_url=project_data["image_url"],
-                    url=project_data["url"]
-                )
-                skill_ids = [Skill.objects.get(name=skill_name).id for skill_name in project_data["skills"]]
-                project.skills.add(*skill_ids)
-                self.stdout.write(f"Projet créé (ID: {project.id})")
+                # Création des sections A propos
+                self.stdout.write("Création des sections À propos...")
+                for content in person["about"]:
+                    about = About.objects.create(profile=profile, content=content)
+                    self.stdout.write(f"Section À propos créée (ID: {about.id})")
 
-            self.stdout.write("Initialisation de la base de données terminée avec succès!")
+                # Création des expériences
+                self.stdout.write("Création des expériences...")
+                for exp_data in person["experiences"]:
+                    exp = Experience.objects.create(
+                        profile=profile,
+                        dates=exp_data["dates"],
+                        position=exp_data["position"],
+                        company=exp_data["company"],
+                        location=exp_data["location"],
+                        description=exp_data["description"],
+                        details=exp_data["details"],
+                        url=exp_data["url"]
+                    )
+                    skill_ids = [Skill.objects.get(name=skill_name).id for skill_name in exp_data["skills"]]
+                    exp.skills.add(*skill_ids)
+                    self.stdout.write(f"Expérience créée (ID: {exp.id})")
+
+                # Création des éducations
+                self.stdout.write("Création des éducations...")
+                for edu_data in person["educations"]:
+                    edu = Education.objects.create(
+                        profile=profile,
+                        dates=edu_data["dates"],
+                        title=edu_data["title"],
+                        institution=edu_data["institution"],
+                        field=edu_data["field"],
+                        location=edu_data["location"],
+                        description=edu_data["description"],
+                        details=edu_data["details"],
+                        url=edu_data["url"]
+                    )
+                    skill_ids = [Skill.objects.get(name=skill_name).id for skill_name in edu_data["skills"]]
+                    edu.skills.add(*skill_ids)
+                    self.stdout.write(f"Éducation créée (ID: {edu.id})")
+
+                # Création des projets
+                self.stdout.write("Création des projets...")
+                for project_data in person["projects"]:
+                    project = Project.objects.create(
+                        profile=profile,
+                        title=project_data["title"],
+                        description=project_data["description"],
+                        details=project_data["details"],
+                        image_url=project_data["image_url"],
+                        url=project_data["url"]
+                    )
+                    skill_ids = [Skill.objects.get(name=skill_name).id for skill_name in project_data["skills"]]
+                    project.skills.add(*skill_ids)
+                    self.stdout.write(f"Projet créé (ID: {project.id})")
+
+                self.stdout.write("Initialisation de la base de données terminée avec succès!")
 
         except Exception as e:
             self.stderr.write(f"Error: {str(e)}")
